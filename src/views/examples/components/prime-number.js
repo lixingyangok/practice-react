4122b0aa7d54ca15247fb6c081b22b8630c7270e
lixingyang <176840078@qq.com>
2019-10-26 21:46:35 +0800

    ts接口

--- src/views/examples/components/prime-number.js
+++ src/views/examples/components/prime-number.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function(){
     let arr = [1,2,3];
-    for(let idx of Array(100).keys() ){
-        let num = idx+1;
-        console.log(num);
-    }
+    // for(let idx of Array(100).keys() ){
+    //     let num = idx+1;
+    //     console.log(num);
+    // }
     return <div>
         {arr.join('，')}
     </div>
